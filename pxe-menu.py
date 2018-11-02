deb8b1c810c1e334766ad09d9fa027191ae9e907
James Wang <jnwang@suse.com>
2018-11-02 14:31:18 +0800

    basic code testing

--- pxe-menu.py
+++ pxe-menu.py
@@ -2,6 +2,16 @@
 
 import os
 
-rootDir = '.'
+rootDir = ".."
 
-
+for root, dirs, files in os.walk(rootDir):
+    for product in dirs:
+        if "Server" not in product:
+            continue
+        for root, dirs, files in os.walk(os.path.join(root, product)):
+            if "x86_64" not in arch:
+                continue
+            for arch in dirs:
+                for root, dirs, files in os.walk(os.path.join(root, arch)):
+                    for dvd in dirs:
+                        print(os.path.join(root, dvd))
